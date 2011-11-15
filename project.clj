af9e021e0eb07e56a4875a9ae6acf5fecb7d0619
Homer Strong <homer.strong@gmail.com>
2011-11-14 17:37:26 -0800

    add clojars name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "1.0.0-SNAPSHOT"
+(defproject org.clojars.strongh/compojure "1.0.0-SNAPSHOT"
   :description "A concise web framework for Clojure"
   :url "https://github.com/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
